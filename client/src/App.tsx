e45ccbf4f3b7b2be6189f49b3a77f414e0978ad8
Ricky <jacobus.kim@gmail.com>
2023-05-24 13:46:10 +0200

    add client template

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 style={{ fontFamily: 'sans-serif' }}>
+        react+tailwind+prettier+eslint+typscript+
+      </h2>
+    </>
+  )
+}
+
+export default App
