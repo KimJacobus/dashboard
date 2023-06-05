d802b61f823f090ab1daefd7231fee54cbeeb472
Ricky <jacobus.kim@gmail.com>
2023-06-05 16:22:40 +0200

    types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLBoolean, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql
 
@@ -10,4 +11,30 @@ const PersonType = new GraphQLObjectType({
     gender: { type: GraphQLString },
     available: { type: GraphQLBoolean },
   }),
+})
+
+const GenderType = new GraphQLObjectType({
+  name: 'Gender',
+  fields: () => ({
+    male: { type: GraphQLString },
+    female: { type: GraphQLString },
+    nonBinary: { type: GraphQLString },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    person: {
+      type: PersonType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(persons, { id: args.id })
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 })
